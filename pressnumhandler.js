3ccca89ff0a19f3741c56768bbf42473e62ae90a
Brian Balayon <brianbalayon@gmail.com>
2020-09-16 12:43:45 -0400

    Added anumber handler for decimals #6

--- pressnumhandler.js
+++ pressnumhandler.js
@@ -1,10 +1,23 @@
 /**
  * @param currentOperand: The number currently in the calculator being entered
  * @param pressedNum: The number corresponding to the button pressed
- * @return The resulting number after entering the new digit
+ * @return number after entering the new digit
  */
-function pressedNum(currentOperand, pressedNum) {
+export function pressedNum(currentOperand, pressedNum) {
     let retVal = currentOperand * 10;
     retVal = retVal + pressedNum;
     return retVal;
+}
+
+/**
+ * @param currentOperand: The number currently in the calculator being entered
+ * @param pressedNum: The number corresponding to the button pressed
+ * @param decimals: The number of decimal places in the number
+ * @return number after entering the new digit
+ */
+export function pressedNumDecimal(currentOperand, pressedNum, decimals) {
+    let power = Math.pow(10, decimals+1);
+    let retVal = currentOperand * power;
+    retVal = retVal + pressedNum;
+    return retVal / power;
 }
