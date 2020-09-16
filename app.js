aa9f96b9c266cb08f5d88b64cd0f8f225e20ee63
Claire <claire.cy.t@gmail.com>
2020-09-16 16:24:03 -0400

    Add kimani's AC logic

--- app.js
+++ app.js
@@ -7,113 +7,124 @@
 
 //no order of operations
 
-// Allows the DOM to load before JavaScript is executed
-window.onload = function () {
+const calculator = {
+  currentTotal: 0,
+  previousTotal: null,
+  operand: null,
+  operator: null
+}
+let {currentTotal, previousTotal, operand, operator} = calculator;
 
-  // Selects the button elements with the classname number
-  document.querySelectorAll('.number').forEach(item => {
+document.addEventListener('click', (event) => {
+  //only allow click on button
+  const {target} = event;
 
-    // Applies the eventListener to all elements with the classname number on click
-    item.addEventListener('click', event => {
+  if (!target.matches('button')) return;
 
-      // Changes the button with the id AC HTML from "AC" to "C"
-      document.getElementById('AC').innerHTML = 'C';
-    });
-  });
+  if (target.className.includes('single number')) {
+    operand = operand ? operand + target.value: target.value;
+    console.log(operand)
+    document.getElementById("screen").innerText = operand;
+  }
 
-  // Selects the button element with the id AC on click
-  document.getElementById('AC').addEventListener('click', function () {
+  if (target.className.includes('single gray')) {
 
-    // Clears all numbers input in the screen id
+    if(target.id.includes("AC")){
     document.getElementById('screen').innerHTML = '0';
-
+    
     // Changes the button from "C" to "AC" after clearing all numbers
     document.getElementById('AC').innerHTML = 'AC';
 
     // Clears the number input history
-    operand = 0;
-
-
-  });
-
-
-  const calculator = {
-    currentTotal: 0,
-    previousTotal: null,
-    operand: null,
-    operator: null
-  }
-  let { currentTotal, previousTotal, operand, operator } = calculator;
-
-  document.addEventListener('click', (event) => {
-    //only allow click on button
-    const { target } = event;
-
-    if (!target.matches('button')) return;
-
-    if (target.className.includes('single number')) {
-      operand = operand ? operand + target.value : target.value;
-      console.log(operand)
-      document.getElementById("screen").innerText = operand;
-
-    }
-
-    if (target.className.includes('single op') && !target.id.includes("equals")) {
-      operator = target.value;
-      if (!previousTotal) previousTotal = operand;
-
+      currentTotal = 0;
+      previousTotal =null;
       operand = null;
-      console.log(operator);
-      // debugger
+      operator = null;
     }
+  }
 
-    if (target.id.includes("equals")) {
+  if (target.className.includes('single op') && !target.id.includes("equals")) {
 
-      operand = target.value;
-      console.log(operand)
+    operand = Number(operand);
+    operator = target.value;
+    if (!previousTotal) {
+      previousTotal = operand;
+    } else {
       switch (operator) {
         case "+": {
-          currentTotal = previousTotal + operand;
-          previousTotal = currentTotal;
-          // reset operator
-          operator = null;
+          previousTotal = previousTotal + operand;
           document.querySelector("#screen").innerHTML = currentTotal;
-          console.log(`Equals ${currentTotal}`);
           break;
         }
         case "-": {
-          currentTotal = previousTotal - operand;
-          previousTotal = currentTotal;
-          // reset operator
-          operator = null;
+          previousTotal = previousTotal - operand;
           document.querySelector("#screen").innerHTML = currentTotal;
-          console.log(`Equals ${currentTotal}`);
           break;
         }
         case "*": {
-          currentTotal = previousTotal * operand;
-          previousTotal = currentTotal;
-          // reset operator
-          operator = null;
+          previousTotal = previousTotal * operand;
           document.querySelector("#screen").innerHTML = currentTotal;
-          console.log(`Equals ${currentTotal}`);
           break;
         }
         case "/": {
-          currentTotal = previousTotal / operand;
-          previousTotal = currentTotal;
-          // reset operator
-          operator = null;
+          previousTotal = previousTotal / operand;
           document.querySelector("#screen").innerHTML = currentTotal;
-          console.log(`Equals ${currentTotal}`);
           break;
         }
-        default: {
-          break;
-        }
-
+        
       }
     }
 
-  })
-}
+    operand = null;
+    console.log(operator);
+  } 
+
+  if (target.id.includes("equals")) {
+    operand = Number(operand);
+
+    switch (operator) {
+      case "+": {
+        currentTotal = previousTotal + operand;
+        previousTotal = currentTotal;
+        // reset operator
+        operator = null;
+        document.querySelector("#screen").innerHTML = currentTotal;
+        console.log(`Equals ${currentTotal}`);
+        break;
+      }
+      case "-": {
+        currentTotal = previousTotal - operand;
+        previousTotal = currentTotal;
+        // reset operator
+        operator = null;
+        document.querySelector("#screen").innerHTML = currentTotal;
+        console.log(`Equals ${currentTotal}`);
+        break;
+      }
+      case "*": {
+        currentTotal = previousTotal * operand;
+        previousTotal = currentTotal;
+        // reset operator
+        operator = null;
+        document.querySelector("#screen").innerHTML = currentTotal;
+        console.log(`Equals ${currentTotal}`);
+        break;
+      }
+      case "/": {
+        currentTotal = previousTotal / operand;
+        previousTotal = currentTotal;
+        // reset operator
+        operator = null;
+        document.querySelector("#screen").innerHTML = currentTotal;
+        console.log(`Equals ${currentTotal}`);
+        break;
+      }
+      default: {
+        break;
+      }
+
+    
+    }
+  }
+
+})
