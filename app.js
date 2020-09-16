9d42525140e80d423895ae972a89d0daaaf0eeb4
kimani kelly <kimanikelly@yahoo.com>
2020-09-16 14:54:33 -0400

    Fixed the clear function

--- app.js
+++ app.js
@@ -7,83 +7,115 @@
 
 //no order of operations
 
-const calculator = {
-  currentTotal: 0,
-  previousTotal: null,
-  operand: null,
-  operator: null
-}
-let {currentTotal, previousTotal, operand, operator} = calculator;
+// Allows the DOM to load before JavaScript is executed
+window.onload = function () {
 
-document.addEventListener('click', (event) => {
-  //only allow click on button
-  const {target} = event;
 
-  if (!target.matches('button')) return;
+  // Selects the button elements with the classname number
+  document.querySelectorAll('.number').forEach(item => {
 
-  if (target.className.includes('single number')) {
-    operand = operand ? operand + target.value: target.value;
-    console.log(operand)
-    document.getElementById("screen").innerText = operand;
-    
+    // Applies the eventListener to all elements with the classname number on click
+    item.addEventListener('click', event => {
+
+      // Changes the button with the id AC HTML from "AC" to "C"
+      document.getElementById('AC').innerHTML = 'C';
+    });
+  });
+
+  // Selects the button element with the id AC on click
+  document.getElementById('AC').addEventListener('click', function () {
+
+    // Clears all numbers input in the screen id
+    document.getElementById('screen').innerHTML = '0';
+
+    // Changes the button from "C" to "AC" after clearing all numbers
+    document.getElementById('AC').innerHTML = 'AC';
+
+    // Clears the number input history
+    operand = 0;
+
+
+  });
+
+
+  const calculator = {
+    currentTotal: 0,
+    previousTotal: null,
+    operand: null,
+    operator: null
   }
+  let { currentTotal, previousTotal, operand, operator } = calculator;
 
-  if (target.className.includes('single op') && !target.id.includes("equals")) {
-    operator = target.value;
-    if (!previousTotal) previousTotal = operand;
+  document.addEventListener('click', (event) => {
+    //only allow click on button
+    const { target } = event;
 
-    operand = null;
-    console.log(operator);
-    // debugger
-  } 
+    if (!target.matches('button')) return;
 
-  if (target.id.includes("equals")) {
+    if (target.className.includes('single number')) {
+      operand = operand ? operand + target.value : target.value;
+      console.log(operand)
+      document.getElementById("screen").innerText = operand;
 
-    operand = target.value;
-    console.log(operand)
-    switch (operator) {
-      case "+": {
-        currentTotal = previousTotal + operand;
-        previousTotal = currentTotal;
-        // reset operator
-        operator = null;
-        document.querySelector("#screen").innerHTML = currentTotal;
-        console.log(`Equals ${currentTotal}`);
-        break;
-      }
-      case "-": {
-        currentTotal = previousTotal - operand;
-        previousTotal = currentTotal;
-        // reset operator
-        operator = null;
-        document.querySelector("#screen").innerHTML = currentTotal;
-        console.log(`Equals ${currentTotal}`);
-        break;
-      }
-      case "*": {
-        currentTotal = previousTotal * operand;
-        previousTotal = currentTotal;
-        // reset operator
-        operator = null;
-        document.querySelector("#screen").innerHTML = currentTotal;
-        console.log(`Equals ${currentTotal}`);
-        break;
-      }
-      case "/": {
-        currentTotal = previousTotal / operand;
-        previousTotal = currentTotal;
-        // reset operator
-        operator = null;
-        document.querySelector("#screen").innerHTML = currentTotal;
-        console.log(`Equals ${currentTotal}`);
-        break;
-      }
-      default: {
-        break;
-      }
-
-    
     }
-  }
 
-})
+    if (target.className.includes('single op') && !target.id.includes("equals")) {
+      operator = target.value;
+      if (!previousTotal) previousTotal = operand;
+
+      operand = null;
+      console.log(operator);
+      // debugger
+    }
+
+    if (target.id.includes("equals")) {
+
+      operand = target.value;
+      console.log(operand)
+      switch (operator) {
+        case "+": {
+          currentTotal = previousTotal + operand;
+          previousTotal = currentTotal;
+          // reset operator
+          operator = null;
+          document.querySelector("#screen").innerHTML = currentTotal;
+          console.log(`Equals ${currentTotal}`);
+          break;
+        }
+        case "-": {
+          currentTotal = previousTotal - operand;
+          previousTotal = currentTotal;
+          // reset operator
+          operator = null;
+          document.querySelector("#screen").innerHTML = currentTotal;
+          console.log(`Equals ${currentTotal}`);
+          break;
+        }
+        case "*": {
+          currentTotal = previousTotal * operand;
+          previousTotal = currentTotal;
+          // reset operator
+          operator = null;
+          document.querySelector("#screen").innerHTML = currentTotal;
+          console.log(`Equals ${currentTotal}`);
+          break;
+        }
+        case "/": {
+          currentTotal = previousTotal / operand;
+          previousTotal = currentTotal;
+          // reset operator
+          operator = null;
+          document.querySelector("#screen").innerHTML = currentTotal;
+          console.log(`Equals ${currentTotal}`);
+          break;
+        }
+        default: {
+          break;
+        }
+
+
+      }
+    }
+
+  })
+}
