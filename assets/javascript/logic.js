2ad702cd5d8d1ffa148864f69c77b2e8500b13c0
kimani kelly <kimanikelly@yahoo.com>
2020-09-16 13:10:06 -0400

    When a number or decimal is selected the AC button changes to C

---
+++ assets/javascript/logic.js
@@ -0,0 +1,20 @@
+// Allows the DOM to load before JavaScript is executed
+window.onload = function () {
+
+    // Selects button elements with the classname number
+    document.querySelectorAll('.number').forEach(item => {
+
+        // Applies the eventListener to all elements with the classname number
+        item.addEventListener('click', event => {
+
+            // Changes the button with the id AC HTML from "AC" to "C"
+            document.getElementById('AC').innerHTML = 'C';
+        })
+    })
+
+
+
+}
+
+
+
