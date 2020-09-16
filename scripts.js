84c3308f957196f9c68c2879f497b86099850449
wwhite11 <2winstonrwhite@gmail.com>
2020-09-16 13:51:10 -0400

    stuff

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function getStoredNumber() {
-    console.log(document.getElementById('screen').innerText);
-    return document.getElementById('screen').innerText;
+  console.log(document.getElementById("screen").innerText);
+  return document.getElementById("screen").innerText;
 }
 
 var singleNumber = document.getElementsByClassName("single number");
 for (var i = 0; i < singleNumber.length; i++) {
-    singleNumber[i].addEventListener('click', function () {
-        document.getElementById("screen").innerText = this.id;
-    });
+  singleNumber[i].addEventListener("click", function () {
+    document.getElementById("screen").innerText = this.id;
+  });
 }
